5f8d8dc8f91699f0af2e91094168134581287129
Amar Singh <amarkirpaa@gmail.com>
2025-01-14 17:18:40 +0530

    separate out the chain data into another file

---
+++ src/data/chains.ts
@@ -0,0 +1,69 @@
+// Cryptocurrency data as a constant
+export const CRYPTOCURRENCIES = [
+  {
+    name: "Bitcoin",
+    code: "BTC",
+    color: "#F7931A",
+    logo: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg",
+  },
+  {
+    name: "Ethereum",
+    code: "ETH",
+    color: "#627EEA",
+    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.svg",
+  },
+  {
+    name: "Avalanche",
+    code: "AVAX",
+    color: "#E84142",
+    logo: "https://cryptologos.cc/logos/avalanche-avax-logo.svg",
+  },
+  {
+    name: "Binance Coin",
+    code: "BNB",
+    color: "#F3BA2F",
+    logo: "https://cryptologos.cc/logos/bnb-bnb-logo.svg",
+  },
+  {
+    name: "Cardano",
+    code: "ADA",
+    color: "#0033AD",
+    logo: "https://cryptologos.cc/logos/cardano-ada-logo.svg",
+  },
+  {
+    name: "Solana",
+    code: "SOL",
+    color: "#14F195",
+    logo: "https://cryptologos.cc/logos/solana-sol-logo.svg",
+  },
+  {
+    name: "Polkadot",
+    code: "DOT",
+    color: "#E6007A",
+    logo: "https://cryptologos.cc/logos/polkadot-new-dot-logo.svg",
+  },
+  {
+    name: "Ripple",
+    code: "XRP",
+    color: "#23292F",
+    logo: "https://cryptologos.cc/logos/xrp-xrp-logo.svg",
+  },
+  {
+    name: "Dogecoin",
+    code: "DOGE",
+    color: "#C2A633",
+    logo: "https://cryptologos.cc/logos/dogecoin-doge-logo.svg",
+  },
+  {
+    name: "Polygon",
+    code: "MATIC",
+    color: "#8247E5",
+    logo: "https://cryptologos.cc/logos/polygon-matic-logo.svg",
+  },
+  {
+    name: "Chainlink",
+    code: "LINK",
+    color: "#2A5ADA",
+    logo: "https://cryptologos.cc/logos/chainlink-link-logo.svg",
+  },
+] as const;
